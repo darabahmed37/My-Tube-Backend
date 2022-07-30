68ada094942bca27cec7c6dc16e4d381c90c3a32
Darab Ahmed <darabahmed37@gmail.com>
2022-07-30 16:24:10 +0500

    .env init

--- arbiosft_project/settings.py
+++ arbiosft_project/settings.py
@@ -76,14 +76,16 @@ WSGI_APPLICATION = 'arbiosft_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'arbisoft_project',
+        'USER': os.getenv("DB_USERNAME"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': int(os.getenv("DB_PORT")),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
