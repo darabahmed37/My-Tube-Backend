e0f8939d6eb7e0e50f6896ba44f7c93de41ddec9
Darab Ahmed <darabahmed37@gmail.com>
2022-08-21 21:46:42 +0500

    Fixed Exception at get all playlist

--- youtube_playlist/urls.py
+++ youtube_playlist/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import FetchPlayList, GetPlayListItems, GetVideoInfo
+from .views import FetchPlayList, GetPlayListItems, GetVideoInfo,SetUserPlaylist
 
 urlpatterns = [
     path("all-playlist/", FetchPlayList.as_view(), name="fetch_playlist"),
     path("list/<str:playlist_id>/", GetPlayListItems.as_view(), name="fetch_playlist-item"),
     path("video/<str:video_id>/", GetVideoInfo.as_view(), name="fetch_video_info"),
+    path("set-playlist/", SetUserPlaylist.as_view(), name="set_user_playlist"),
 ]
