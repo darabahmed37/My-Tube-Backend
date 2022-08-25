c4ea437cc387bcd754dbc0cda6dc604acf7b36cc
Darab Ahmed <darabahmed37@gmail.com>
2022-08-25 08:51:40 +0500

    User Auth

--- arbisoft_project/settings.py
+++ arbisoft_project/settings.py
@@ -146,7 +146,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = default_headers + (
     'Access-Control-Allow-Origin',
 )
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=100),
-}
+#
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(seconds=5),
+# }
