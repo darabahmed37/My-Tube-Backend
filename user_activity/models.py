c07cfcb6b3fe159629fbb95c0ef2bd1936cd63d0
Darab Ahmed <darabahmed37@gmail.com>
2022-08-25 10:58:48 +0500

    new models

--- user_activity/models.py
+++ user_activity/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 class Timer(models.Model):
     total_time = models.FloatField(default=5)
-    used_time = models.FloatField(default=0)
-    date = models.DateTimeField(auto_now=True)
+    date_time = models.DateTimeField(auto_now_add=True)
 
 
 class WatchTiming(models.Model):
-    user = models.OneToOneField('authentication.User', on_delete=models.CASCADE, primary_key=True)
-    current_timer = models.OneToOneField(Timer, on_delete=models.CASCADE, related_name='current_timer')
-    previous_timers = models.ForeignKey(Timer, on_delete=models.CASCADE, related_name="previous_timers", null=True)
+    user = models.ForeignKey("authentication.User", on_delete=models.CASCADE, primary_key=True)
+    current_timer = models.ForeignKey(Timer, on_delete=models.CASCADE, related_name="current_timer")
+    previous_timers = models.ForeignKey(Timer, on_delete=models.CASCADE, related_name="previous_timers")
--- user_activity/serializer.py
+++ user_activity/serializer.py
@@ -1,43 +1,15 @@
-from datetime import datetime
-
 from rest_framework import serializers
 
-from user_activity.models import Timer, WatchTiming
+from .models import Timer
 
 
 class TimerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timer
         exclude = ("id",)
-        partial = True
 
 
 class WatchTimingSerializer(serializers.ModelSerializer):
-    current_timer = TimerSerializer(default=True)
-    previous_timers = TimerSerializer(many=True, read_only=True)
+    current_timer = TimerSerializer(default=Timer.objects.create())
+    previous_timers = TimerSerializer(many=True)
     user = serializers.PrimaryKeyRelatedField(read_only=True)
-
-    class Meta:
-        model = WatchTiming
-        fields = ("current_timer", "previous_timers", "user")
-
-    def create(self, validated_data):
-        if "current_timer" in validated_data:
-            current_timer_data = validated_data.pop("current_timer")
-            current_timer = Timer.objects.create(**current_timer_data)
-        else:
-            current_timer = Timer.objects.create()
-
-        watch_timing = WatchTiming.objects.create(user=self.context["request"].user, current_timer=current_timer,
-                                                  previous_timers=None)
-        return watch_timing
-
-    def update(self, instance: WatchTiming, validated_data):
-        if instance.current_timer.date.date() < datetime.now().date():
-            instance.previous_timers.add(instance.current_timer)
-            instance.current_timer = Timer.objects.create()
-
-        elif "updated_time" in self.context["request"].data and instance.current_timer.used_time < instance.current_timer.total_time:
-            instance.current_timer.used_time += self.context["request"].data["updated_time"]
-        instance.current_timer.save()
-        return instance
