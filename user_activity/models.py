037a53edfe4b094e027e41f135a27c082c956380
Darab Ahmed <darabahmed37@gmail.com>
2022-08-29 17:33:04 +0500

    Added Increased Time

--- user_activity/models.py
+++ user_activity/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class Timer(models.Model):
-    total_time = models.FloatField(default=5)
+    total_time = models.FloatField(default=2)
     date = models.DateTimeField(auto_now=True)
     user = models.OneToOneField('authentication.User', on_delete=models.CASCADE)
+    availed_time = models.BooleanField(default=False)
 
 
 class PreviousTimers(models.Model):
-    total_time = models.FloatField(default=5)
+    total_time = models.FloatField(default=2)
     date = models.DateTimeField()
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
+    availed_time = models.BooleanField(default=False)
